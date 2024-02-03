623a34b3dff570abe0b62582c1e50dd16ae4fcc1
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-02-03 20:11:24 +0600

    first commit

---
+++ server/routes/users.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, update } from '../controllers/user.js';
+import { verifyToken } from '../verifyToken.js';
+
+const router = express.Router();
+
+// UPDATE USER
+// router.put("/:id", verifyToken, update)
+
+// DELETE USER
+router.delete("/:id", deleteUser)
+
+// GET A USER
+router.get("/find/:id", getUser)
+
+// SUBSCRIBE A USER
+router.put("/sub/:id", subscribe)
+
+// UNSUBSCRIBE A USER
+router.put("/unsub/:id", unsubscribe)
+
+// LIKE A VIDEO
+router.put("/like/:videoId", like)
+
+// DESLIKE A VIDEO
+router.put("/dislike/:videoId", dislike)
+
+export default router;
